01617f676309387e75fef025776fedbf331dcb30
Rzerooooo <s1100661@gm.pu.edu.tw>
2024-05-14 14:14:47 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ def load_data(url):
 	return df
 
 df_original = load_data("kbars_2330_2022-01-01-2022-11-18.pkl")
-df_original = pd.read_pickle('kbars_2330_2022-01-01-2022-11-18.pkl')
+#df_original = pd.read_pickle('kbars_2330_2022-01-01-2022-11-18.pkl')
 
 #df.columns  ## Index(['Unnamed: 0', 'time', 'open', 'low', 'high', 'close', 'volume','amount'], dtype='object')
 df_original = df_original.drop('Unnamed: 0',axis=1)
